622b8f4f3d6efa9cdf5ef302aa72f339fe56457e
Marge <marge.consunji@gmail.com>
2022-03-29 16:47:57 +0800

    Add page header

---
+++ pages/panels.js
@@ -0,0 +1,16 @@
+import PageHeader from '../components/layout/PageHeader';
+
+const Panels = () => {
+  return (
+    <>
+      <PageHeader pageTitle="Panels">
+        Join any of our five fascinating panels and get to know know more about
+        politics and international relations in the Asia-Pacific region.
+      </PageHeader>
+
+      <section className="container"></section>
+    </>
+  );
+};
+
+export default Panels;
