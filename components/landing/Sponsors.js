eb1d8082f021c50182dc5e2dc2100f7f7bf9d836
Marge <marge.consunji@gmail.com>
2022-03-29 15:57:09 +0800

    Make Sponsor section

--- components/landing/Sponsors.js
+++ components/landing/Sponsors.js
@@ -1,7 +1,36 @@
+import Image from 'next/image';
+import moeLogo from '../../public/assets/images/sponsors/moe-transparent.png';
+import mostLogo from '../../public/assets/images/sponsors/most-transparent.png';
+import nccuLogo from '../../public/assets/images/sponsors/nccu-transparent.png';
+import demoLogo from '../../public/assets/images/sponsors/tw-foundation-for-democracy-transparent.png';
+
+const Sponsor = ({ path, altText, small }) => {
+  return (
+    <div className={`${small ? 'max-w-[6rem]' : ''}`}>
+      <Image src={path} alt={altText} placeholder="blur" />
+    </div>
+  );
+};
+
 const Sponsors = () => {
   return (
-    <section>
-      <div className="container">Sponsors go here</div>
+    <section className="bg-blue100 text-white">
+      <div className="container">
+        <p className="uppercase">Proudly sponsored by</p>
+        <div className="flex items-center justify-center gap-8">
+          <Sponsor path={moeLogo} alt="Ministry of Education Logo" small />
+          <Sponsor
+            path={mostLogo}
+            alt="Ministry of Science and Technology Logo"
+          />
+          <Sponsor
+            path={nccuLogo}
+            alt="National Chengchi University Logo"
+            small
+          />
+          <Sponsor path={demoLogo} alt="Taiwan Foundation for Democracy Logo" />
+        </div>
+      </div>
     </section>
   );
 };
