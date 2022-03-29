9714bd75b80b0e85cf134d334a17087678e9bfd2
Marge <marge.consunji@gmail.com>
2022-03-29 13:20:54 +0800

    Add sans serif option, container config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,18 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
-
   theme: {
+    fontFamily: {
+      serif: ['Playfair Display', 'serif'],
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+      },
+    },
     extend: {},
   },
   plugins: [],
