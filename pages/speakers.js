684cf7c9f8803ceaef558f09bb54146e6a15d55c
Marge <marge.consunji@gmail.com>
2022-03-30 17:01:40 +0800

    Add content to page, style grid and images

---
+++ pages/speakers.js
@@ -0,0 +1,91 @@
+import getData from '../utils/getData';
+import getMarkdown from '../utils/getMarkdown';
+import PageHeader from '../components/layout/PageHeader';
+import Image from 'next/image';
+
+const Speaker = ({ name, title, bio, photo, profileLink }) => {
+  return (
+    <div>
+      <div className="mb-5 overflow-hidden">
+        <div className="transition ease-in duration-200 grayscale hover:scale-110 hover:filter-none">
+          <Image
+            src={photo.url}
+            width={photo.width}
+            height={photo.height}
+            alt={title}
+            layout="responsive"
+            objectFit="contain"
+          />
+        </div>
+      </div>
+      <div className="">
+        <p className="font-serif text-2xl mb-2 text-magenta max-w-[80%]">
+          {name}
+        </p>
+        <p className="text-sm max-w-[80%]">{title}</p>
+        {/* <div className="wave-bg w-full h-4" /> */}
+      </div>
+    </div>
+  );
+};
+
+const Speakers = ({ speakersSource }) => {
+  console.log(speakersSource);
+  return (
+    <>
+      <PageHeader pageTitle="Speakers">
+        This year&apos;s international conference features an exciting line-up
+        of speakers with a wide range of experience in their respective fields
+        of research. We can&apos;t wait for you to meet them.
+      </PageHeader>
+      <section className="container gap-8 grid sm:grid-cols-2 md:grid-cols-3">
+        {speakersSource.map(
+          ({
+            speakerName,
+            id,
+            speakerBio,
+            speakerTitle,
+            speakerProfileLink,
+            photo,
+          }) => (
+            <Speaker
+              key={id}
+              name={speakerName}
+              title={speakerTitle}
+              photo={photo}
+            />
+          )
+        )}
+      </section>
+    </>
+  );
+};
+
+export default Speakers;
+
+export async function getStaticProps() {
+  const query = `{
+    speakers {
+      id
+      speakerBio
+      speakerName
+      speakerTitle
+      speakerProfileLink
+      photo {
+        url(transformation: {})
+        height
+        width
+      }
+    }
+  }
+  `;
+
+  const { speakers } = await getData(query);
+  const speakersSource = await getMarkdown(speakers, 'speakerBio');
+
+  return {
+    props: {
+      speakersSource,
+    },
+  };
+}
