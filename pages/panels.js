2cced8377603494c0acc6848bd6c87242871face
Marge <marge.consunji@gmail.com>
2022-03-30 13:51:26 +0800

    Pull data from GraphCMS and render
    - Style panel write-ups paragraphs
    - Allow for toggling between panel topics
    - Render markdown with next-mdx-remote

--- pages/panels.js
+++ pages/panels.js
@@ -1,7 +1,17 @@
+import { useState } from 'react';
 import getData from '../utils/getData';
+import { serialize } from 'next-mdx-remote/serialize';
+import { MDXRemote } from 'next-mdx-remote';
 import PageHeader from '../components/layout/PageHeader';
 
-const Panels = ({ panels }) => {
+const Panels = ({ panelsSource }) => {
+  console.log(panelsSource);
+  const [content, setContent] = useState('Democracy and Governance Innovation');
+
+  let filteredPanel = panelsSource.filter(
+    (panel) => panel.panelTitle === content
+  );
+
   return (
     <>
       <PageHeader pageTitle="Panels">
@@ -9,7 +19,20 @@ const Panels = ({ panels }) => {
         politics and international relations in the Asia-Pacific region.
       </PageHeader>
 
-      <section className="container"></section>
+      <section className="container gap-12 grid md:grid-cols-[220px_1fr]">
+        <div>
+          <h3>Topics</h3>
+          {panelsSource.map(({ id, panelTitle }) => (
+            <div key={id} onClick={() => setContent(panelTitle)}>
+              {panelTitle}
+            </div>
+          ))}
+        </div>
+        <div className="content">
+          <h2>{filteredPanel[0].panelTitle}</h2>
+          <MDXRemote {...filteredPanel[0].panelWriteup} />
+        </div>
+      </section>
     </>
   );
 };
@@ -17,19 +40,26 @@ const Panels = ({ panels }) => {
 export default Panels;
 
 export async function getStaticProps() {
-  const query = `
-    {
-      panels {
-        id
-        content
-        panelTitle
-      }
+  const query = `{
+    panels {
+      id
+      panelTitle
+      panelWriteup
     }
+  }
   `;
 
   const { panels } = await getData(query);
 
+  const panelsSource = await Promise.all(
+    panels.map(async (panel) => {
+      return { ...panel, panelWriteup: await serialize(panel.panelWriteup) };
+    })
+  );
+
   return {
-    props: { panels },
+    props: {
+      panelsSource,
+    },
   };
 }
