11397ad22a4566cd83dbd851617aeabe6b0ed0aa
Marge <marge.consunji@gmail.com>
2022-03-31 11:13:43 +0800

    Rename component

--- components/layout/PageGrid.js
+++
@@ -1,9 +0,0 @@
-const PageGrid = ({ children }) => {
-  return (
-    <section className="container max-w-6xl mx-auto md:flex md:justify-end">
-      <div className="max-w-4xl">{children}</div>
-    </section>
-  );
-};
-
-export default PageGrid;
