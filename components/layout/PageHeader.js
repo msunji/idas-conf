fa865e506dd9dcc995c634ccf6373e91aeaa6fe7
Marge <marge.consunji@gmail.com>
2022-03-31 16:49:56 +0800

    Add top margin here instead

--- components/layout/PageHeader.js
+++ components/layout/PageHeader.js
@@ -1,8 +1,8 @@
 const PageHeader = ({ pageTitle, children }) => {
   return (
-    <section className="container max-w-6xl">
+    <section className="container max-w-6xl mt-20 md:mt-40">
       <div className="max-w-prose">
-        <h1 className="text-4xl md:text-6xl">{pageTitle}</h1>
+        <h1 className="mb-8 text-4xl md:text-6xl">{pageTitle}</h1>
         <p>{children}</p>
       </div>
     </section>
