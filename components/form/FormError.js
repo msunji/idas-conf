be6d655dfd42396f596de1e2bcedad9b6f10d5a4
Marge <marge.consunji@gmail.com>
2022-04-01 16:43:59 +0800

    Remove icons, opt for text only

--- components/form/FormError.js
+++ components/form/FormError.js
@@ -1,10 +1,7 @@
-import ErrorIcon from '../../public/assets/images/icons/ErrorIcon';
-
 const FormError = ({ children }) => {
   return (
     <p className="text-sm font-semibold text-magenta tracking-wide flex items-center space-x-1 my-2 uppercase">
-      <ErrorIcon />
-      <span className="ml-4">{children}</span>
+      {children}
     </p>
   );
 };
