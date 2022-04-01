52e371b3304fb4613c58c61242373cfd8d2d737d
Marge <marge.consunji@gmail.com>
2022-04-01 11:17:28 +0800

    Add param for variables

--- utils/getData.js
+++ utils/getData.js
@@ -1,13 +1,13 @@
 import { request, GraphQLClient, gql } from 'graphql-request';
 
-const getData = (query) => {
+const getData = (query, variable = {}) => {
   const client = new GraphQLClient(process.env.GRAPHCMS_ENDPOINT);
 
   const graphQuery = gql`
     ${query}
   `;
 
-  return client.request(graphQuery);
+  return client.request(graphQuery, variable);
 };
 
 export default getData;
