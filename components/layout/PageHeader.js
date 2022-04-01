34bac949f38b5f2c31d18dc1e3f875a10a7245bf
Marge <marge.consunji@gmail.com>
2022-04-01 10:16:08 +0800

    Zhush up component with SVG

--- components/layout/PageHeader.js
+++ components/layout/PageHeader.js
@@ -1,11 +1,28 @@
+const HeaderWave = () => {
+  return (
+    <div className="absolute top-0 left-0 -z-10 w-full overflow-x-hidden">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#f3f4f5"
+          fillOpacity="1"
+          d="M0,192L720,128L1440,256L1440,0L720,0L0,0Z"
+        ></path>
+      </svg>
+    </div>
+  );
+};
+
 const PageHeader = ({ pageTitle, children }) => {
   return (
-    <section className="container max-w-6xl mt-20 md:mt-40">
-      <div className="max-w-prose">
-        <h1 className="mb-8 text-4xl md:text-6xl">{pageTitle}</h1>
-        <p>{children}</p>
-      </div>
-    </section>
+    <header>
+      <HeaderWave />
+      <section className="container max-w-6xl mt-20 md:mt-40">
+        <div className="max-w-prose">
+          <h1 className="mb-8 text-4xl md:text-6xl">{pageTitle}</h1>
+          <p>{children}</p>
+        </div>
+      </section>
+    </header>
   );
 };
 
