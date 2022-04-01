b312fa83e160e634a2ef72b24493a9bd2aa7f7dc
Marge <marge.consunji@gmail.com>
2022-04-01 17:41:57 +0800

    Style footer, fix columns

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -17,41 +17,47 @@ const DropdownLinks = ({ links }) => {
 
 const Footer = () => {
   return (
-    <footer className="bg-blue100 text-white pt-10 pb-20 text-[0.9rem]">
-      <div className="container flex flex-col gap-4 md:flex-row">
-        <div className="basis-1/4">
+    <footer className="bg-blue100 text-white text-[0.9rem] pt-12 md:pt-16 pb-28">
+      <div className="container max-w-7xl w-full grid gap-8 tablet:grid-cols-3">
+        <div>
           <h3>Site Map</h3>
-          <ul>
+          <ul className="columns-2">
             {routes.map(({ page, route }) => (
-              <li key={page}>
-                <Link href={route}>
+              <Link key={page} href={route} passHref>
+                <li>
                   <a>{page}</a>
-                </Link>
-              </li>
+                </li>
+              </Link>
             ))}
           </ul>
         </div>
-        <div className="basis-1/4">
+        <div>
           <h3>Related Links</h3>
           <ul className="footer-links">
-            <li>
-              <a href="https://www.nccu.edu.tw/">
+            <li className="before:content-['—']">
+              —
+              <a className="ml-2" href="https://www.nccu.edu.tw/">
                 National Chengchi University
               </a>
             </li>
-            <li>
-              <a href="https://css.nccu.edu.tw/en/">
+            <li className="before:content-['—']">
+              —
+              <a className="ml-2" href="https://css.nccu.edu.tw/en/">
                 College of Social Sciences
               </a>
             </li>
-            <li>
-              <a href="https://internationalprograms.nccu.edu.tw/home-idas/">
+            <li className="before:content-['—']">
+              —
+              <a
+                className="ml-2"
+                href="https://internationalprograms.nccu.edu.tw/home-idas/"
+              >
                 International Doctoral Program in Asia-Pacific Studies (IDAS)
               </a>
             </li>
           </ul>
         </div>
-        <div className="grow">
+        <div>
           <h3>Contact Us</h3>
           <ul>
             <li>
