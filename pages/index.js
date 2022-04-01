7932ca8b28a19851478bc1337f53234ab3ab0cc6
Marge <marge.consunji@gmail.com>
2022-04-01 17:26:15 +0800

    Add wave border

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,18 @@ import Registration from '../components/landing/Registration';
 import Faqs from '../components/landing/Faqs';
 import Sponsors from '../components/landing/Sponsors';
 
+const LandingWave = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 300">
+      <path
+        fill="#00cba9"
+        fillOpacity="1"
+        d="M0,192L48,181.3C96,171,192,149,288,122.7C384,96,480,64,576,42.7C672,21,768,11,864,10.7C960,11,1056,21,1152,42.7C1248,64,1344,96,1392,112L1440,128L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+      ></path>
+    </svg>
+  );
+};
+
 export default function Home({ faqs }) {
   return (
     <div>
@@ -15,10 +27,13 @@ export default function Home({ faqs }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
+        <div>
+          <LandingWave className="absolute inset" />
+        </div>
         <Hero />
         <Subtitle />
-        <Faqs faqs={faqs} />
         <Registration />
+        <Faqs faqs={faqs} />
         <Sponsors />
       </main>
     </div>
