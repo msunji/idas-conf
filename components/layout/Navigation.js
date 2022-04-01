0d2971ca809c52d2f626a9be4140e0349ac1d6ca
Marge <marge.consunji@gmail.com>
2022-04-01 14:12:44 +0800

    Set nav bg to white on scroll

--- components/layout/Navigation.js
+++ components/layout/Navigation.js
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
-import routes from '../../public/data/routes.json';
+import routes from '/public/data/routes.json';
 import Link from 'next/link';
 
 const useCheckPage = () => {
@@ -13,7 +13,7 @@ const Logo = () => {
     <div className="hidden tablet:flex leading-tight">
       <div className="w-6 h-6 bg-yellow rounded-full mr-2 -mt-2 self-start" />
       <Link href="/">
-        <a className="nav-logo  text-blue-100">
+        <a className="hover:no-underline text-blue-100">
           <span className="tracking-widest font-semibold text-sm leading-[0]">
             2018 IDAS
           </span>
@@ -35,13 +35,37 @@ const RegistrationBtn = () => {
 const Navigation = () => {
   const { pathname } = useRouter();
   const [openMobile, setOpenMobile] = useState(false);
+  const [clientWindowHeight, setClientWindowHeight] = useState('');
+  const [navBg, setNavBg] = useState('bg-transparent');
+  const [navPadding, setNavPadding] = useState('py-8');
 
   const handleMobileToggle = () => setOpenMobile(!openMobile);
   const closeMobile = () => setOpenMobile(false);
   useCheckPage();
 
+  const handleScroll = () => {
+    setClientWindowHeight(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  });
+
+  useEffect(() => {
+    let threshold = clientWindowHeight / 600;
+    console.log(threshold);
+    if (threshold > 0) {
+      setNavBg('bg-white');
+      setNavPadding('py-5');
+    } else {
+      setNavBg('bg-transparent');
+      setNavPadding('py-8');
+    }
+  }, [clientWindowHeight]);
+
   return (
-    <nav className="fixed top-0 left-0 z-10 w-screen">
+    <nav className={`fixed top-0 left-0 z-10 w-screen ${navBg}`}>
       <div className={`container relative tablet:hidden`}>
         <div
           className="absolute right-0 pt-8 px-[inherit]"
@@ -66,7 +90,7 @@ const Navigation = () => {
         </div>
       </div>
       <div
-        className={`container max-w-7xl w-screen py-10 bg-violet h-screen text-white tracking-wide ${
+        className={`container max-w-7xl w-screen ${navPadding} bg-violet h-screen text-white tracking-wide ${
           openMobile ? 'flex flex-col justify-end' : 'hidden'
         } tablet:flex tablet:flex-row tablet:h-auto tablet:justify-between tablet:items-center tablet:text-blue300 tablet:bg-transparent`}
       >
