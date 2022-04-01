41f9b4827c11775e511662e6bfacc12ca111edfc
Marge <marge.consunji@gmail.com>
2022-04-01 12:59:11 +0800

    Fix text container widths, make image and names clickable

--- pages/speakers.js
+++ pages/speakers.js
@@ -1,61 +1,59 @@
 import getData from '../utils/getData';
-import getMarkdown from '../utils/getMarkdown';
 import PageHeader from '../components/layout/PageHeader';
 import PageContent from '../components/layout/PageContent';
 import Image from 'next/image';
+import Link from 'next/link';
 
-const Speaker = ({ name, title, bio, photo, profileLink }) => {
+const Speaker = ({ name, title, photo, slug }) => {
   return (
     <div>
-      <div className="mb-5 overflow-hidden">
-        <div className="transition ease-in duration-200 grayscale hover:scale-110 hover:filter-none">
-          <Image
-            src={photo.url}
-            width={photo.width}
-            height={photo.height}
-            alt={title}
-            layout="responsive"
-            objectFit="contain"
-          />
-        </div>
+      <div className="mb-3 overflow-hidden">
+        <Link href={`/speakers/${slug}`}>
+          <a>
+            <div className="transition ease-in duration-200 grayscale hover:scale-110 hover:filter-none">
+              <Image
+                src={photo.url}
+                width={photo.width}
+                height={photo.height}
+                alt={title}
+                layout="responsive"
+                objectFit="contain"
+              />
+            </div>
+          </a>
+        </Link>
       </div>
-      <div className="">
-        <p className="font-serif text-2xl mb-2 text-magenta max-w-[80%]">
-          {name}
-        </p>
-        <p className="text-sm max-w-[80%]">{title}</p>
+      <div className="px-1">
+        <Link href={`/speakers/${slug}`}>
+          <a className="font-serif text-2xl mb-2 text-magenta hover:underline">
+            {name}
+          </a>
+        </Link>
+        <p className="text-sm">{title}</p>
       </div>
     </div>
   );
 };
 
-const Speakers = ({ speakersSource }) => {
+const Speakers = ({ speakers }) => {
   return (
     <>
-      <PageHeader pageTitle="Speakers" bgColor="violet">
+      <PageHeader pageTitle="Speakers">
         This year&apos;s international conference features an exciting line-up
         of speakers with a wide range of experience in their respective fields
         of research. We can&apos;t wait for you to meet them.
       </PageHeader>
       <PageContent>
-        <div className="gap-8 grid sm:grid-cols-2 md:grid-cols-3">
-          {speakersSource.map(
-            ({
-              speakerName,
-              id,
-              speakerBio,
-              speakerTitle,
-              speakerProfileLink,
-              photo,
-            }) => (
-              <Speaker
-                key={id}
-                name={speakerName}
-                title={speakerTitle}
-                photo={photo}
-              />
-            )
-          )}
+        <div className="gap-8 grid grid-cols-2 md:grid-cols-3">
+          {speakers.map(({ speakerName, id, speakerTitle, photo, slug }) => (
+            <Speaker
+              key={id}
+              name={speakerName}
+              title={speakerTitle}
+              photo={photo}
+              slug={slug}
+            />
+          ))}
         </div>
       </PageContent>
     </>
@@ -73,20 +71,20 @@ export async function getStaticProps() {
       speakerTitle
       speakerProfileLink
       photo {
-        url(transformation: {})
+        url(transformation: {image: {resize: {height: 362, width: 287}}})
         height
         width
       }
+      slug
     }
   }
   `;
 
   const { speakers } = await getData(query);
-  const speakersSource = await getMarkdown(speakers, 'speakerBio');
 
   return {
     props: {
-      speakersSource,
+      speakers,
     },
   };
 }
