c8c063da429c99f2b85c8656cc308686693f419f
Marge <marge.consunji@gmail.com>
2022-04-03 11:17:30 +0800

    Make custom head component

---
+++ components/CustomHead.js
@@ -0,0 +1,40 @@
+import Head from 'next/head';
+
+const CustomHead = ({ pageTitle }) => {
+  return (
+    <Head>
+      <title>
+        2018 IDAS International Conference |{' '}
+        {pageTitle
+          ? `${pageTitle}`
+          : `Transformation and Challenges in Asia Pacific: The Next Decade and Beyond`}
+      </title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta
+        name="description"
+        content="2018 IDAS International Conference. Transformation and Challenges in Asia Pacific: The Next Decade and Beyond. Nov 24-25, 2018 at NCCU, Taipei."
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <meta name="theme-color" content="#ffffff" />
+    </Head>
+  );
+};
+
+export default CustomHead;
