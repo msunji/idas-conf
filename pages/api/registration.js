9e01bb259b60c9809b967b9644401ed204f8d04c
Marge <marge.consunji@gmail.com>
2022-04-06 16:08:30 +0800

    Remove console log, add res status to error

--- pages/api/registration.js
+++ pages/api/registration.js
@@ -1,3 +1,24 @@
+const mail = require('@sendgrid/mail');
+mail.setApiKey(process.env.SENDGRID_REGISTRATION_KEY);
+
 export default function handler(req, res) {
-  res.status(200).json({ status: 'ok' });
+  const { fullName, email } = req.body;
+  const message = `Hello ${fullName}!\r\n
+                   This is to confirm your registration for the 2018 IDAS International Conference happening on November 24-25 at National Chengchi University.
+                  `;
+  mail
+    .send({
+      to: email,
+      from: 'idasconf2018@protonmail.com',
+      subject: `You've successfully registered for the 2018 IDAS International Conference!`,
+      text: message,
+    })
+    .then(() => {
+      console.log('Email sent');
+      res.status(200).json({ status: 'ok', body: req.body });
+    })
+    .catch((err) => {
+      res.json(err);
+      res.status(405);
+    });
 }
