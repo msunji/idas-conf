897791db10fcea326ebdf187e6e25d04b77325e1
Marge <marge.consunji@gmail.com>
2022-04-08 15:56:39 +0800

    Attempt 2 at fixing api error

--- pages/api/registration.js
+++ pages/api/registration.js
@@ -1,21 +1,21 @@
 import postAirtableData from '../../utils/postAirtableData';
 import sendMail from '../../utils/sendMail';
 
-export default async function handler(req, res) {
+export default function handler(req, res) {
   try {
     const { fullName, email } = req.body;
-    await sendMail(
+    sendMail(
       fullName,
       email,
       process.env.SENDGRID_REGISTRATION_TEMPLATE,
       'IDAS Conference Registration Confirmation'
     );
-    await postAirtableData(process.env.REGISTRATION_TAB, {
+    postAirtableData(process.env.REGISTRATION_TAB, {
       'Full Name': fullName,
       Email: email,
     });
-    res.status(200).json({ status: 'ok', body: req.body });
+    return res.status(200).json({ status: 'ok', body: req.body });
   } catch (err) {
-    res.status(450).json({ status: 'error', error: err.message });
+    return res.status(450).json({ status: 'error', error: err.message });
   }
 }
