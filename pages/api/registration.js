171df02ab9102e6f4e8945870064c2eef6864142
Marge <marge.consunji@gmail.com>
2022-04-08 16:31:57 +0800

    Try fixing form error

--- pages/api/registration.js
+++ pages/api/registration.js
@@ -1,22 +1,36 @@
 import postAirtableData from '../../utils/postAirtableData';
 import sendMail from '../../utils/sendMail';
 
+const Airtable = require('airtable');
+
+Airtable.configure({ apiKey: process.env.AIRTABLE_API_KEY });
+const base = Airtable.base(process.env.BASE_ID);
+
 export default function handler(req, res) {
   try {
     const { fullName, email } = req.body;
+    let table = base('Registration');
+    table.create(
+      {
+        'Full Name': fullName,
+        Email: email,
+      },
+      (err) => {
+        if (err) {
+          console.error(err);
+        }
+        return;
+      }
+    );
     sendMail(
       fullName,
       email,
       process.env.SENDGRID_REGISTRATION_TEMPLATE,
       'IDAS Conference Registration Confirmation'
     );
-    postAirtableData('Registration', {
-      'Full Name': fullName,
-      Email: email,
-    });
-    return res.status(200).json({ status: 'ok', body: req.body });
+    res.status(200).json({ status: 'ok', body: req.body });
   } catch (err) {
     console.log(err);
-    return res.status(450).json({ status: 'error', error: err.message });
+    res.status(450).json({ status: 'error', error: err.message });
   }
 }
