42ce87b5e2dc9328ceb40b368fe40f101d32fd66
Marge <marge.consunji@gmail.com>
2022-04-08 14:37:30 +0800

    Add location marker, phone, mail icons

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -23,7 +23,7 @@ const Footer = () => {
     <footer className="bg-blue100 text-white text-[0.9rem] pt-12 md:pt-16 pb-28">
       <div className="container max-w-7xl w-full grid gap-8 tablet:grid-cols-3">
         <div>
-          <h3>Site Map</h3>
+          <h3 className="mb-2">Site Map</h3>
           <ul className="columns-2">
             {routes.map(({ page, route }) => (
               <Link key={page} href={route} passHref>
@@ -35,7 +35,7 @@ const Footer = () => {
           </ul>
         </div>
         <div>
-          <h3>Related Links</h3>
+          <h3 className="mb-2">Related Links</h3>
           <ul className="footer-links">
             <li className="list-item-dash">
               <a className="ml-2" href="https://www.nccu.edu.tw/">
@@ -58,23 +58,26 @@ const Footer = () => {
           </ul>
         </div>
         <div>
-          <h3>Contact Us</h3>
-          <ul>
-            <li>
-              <Location />
-              <span>
-                {' '}
-                No. 64, Section 2, Zhinan Road, Wenshan District, Taipei City,
-                Taiwan
+          <h3 className="mb-2">Contact Us</h3>
+          <ul className="space-y-1">
+            <li className="flex items-center">
+              <span className="mr-2 self-start">
+                <Location width="w-5" height="h-5" />
               </span>
+              No. 64, Section 2, Zhinan Road, Wenshan District, Taipei City,
+              Taiwan
             </li>
-            <li>
-              <Tel />
-              <span>+886-2-2939-3091 Ext. 51278</span>
+            <li className="flex items-center">
+              <span className="mr-2">
+                <Tel width="w-5" height="h-5" />
+              </span>
+              +886-2-2939-3091 Ext. 51278
             </li>
-            <li>
-              <Mail />
-              <span>idas@nccu.edu.tw</span>
+            <li className="flex items-center">
+              <span className="mr-2">
+                <Mail width="w-5" height="h-5" />
+              </span>
+              idas@nccu.edu.tw
             </li>
           </ul>
         </div>
