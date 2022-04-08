16ed94bc00091382fdb29b6b93963b78815e0508
Marge <marge.consunji@gmail.com>
2022-04-08 13:14:34 +0800

    Clean up code, import sendMail function

--- pages/api/registration.js
+++ pages/api/registration.js
@@ -1,31 +1,21 @@
 import postAirtableData from '../../utils/postAirtableData';
-
-const mail = require('@sendgrid/mail');
-
-mail.setApiKey(process.env.SENDGRID_REGISTRATION_KEY);
+import sendMail from '../../utils/sendMail';
 
 export default function handler(req, res) {
-  const { fullName, email } = req.body;
-  const message = `Hello ${fullName}!\r\n
-                   This is to confirm your registration for the 2018 IDAS International Conference happening on November 24-25 at National Chengchi University.
-                  `;
-  postAirtableData(process.env.REGISTRATION_TAB, {
-    'Full Name': fullName,
-    Email: email,
-  });
-  mail
-    .send({
-      to: email,
-      from: 'idasconf2018@protonmail.com',
-      subject: `You've successfully registered for the 2018 IDAS International Conference!`,
-      text: message,
-    })
-    .then(() => {
-      console.log('Email sent');
-      res.status(200).json({ status: 'ok', body: req.body });
-    })
-    .catch((err) => {
-      res.json(err);
-      res.status(405);
+  try {
+    const { fullName, email } = req.body;
+    sendMail(
+      fullName,
+      email,
+      process.env.SENDGRID_REGISTRATION_TEMPLATE,
+      'IDAS Conference Registration Confirmation'
+    );
+    postAirtableData(process.env.REGISTRATION_TAB, {
+      'Full Name': fullName,
+      Email: email,
     });
+    res.status(200).json({ status: 'ok', body: req.body });
+  } catch (err) {
+    res.status(450).json({ status: 'error', error: err.message });
+  }
 }
