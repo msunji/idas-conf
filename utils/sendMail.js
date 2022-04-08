4c876f0d2f5763e4395d03e01e7fd08b8a6b16ea
Marge <marge.consunji@gmail.com>
2022-04-08 12:52:20 +0800

    Add name, email arguments, remove calls to res.status, res.json

---
+++ utils/sendMail.js
@@ -0,0 +1,25 @@
+const mail = require('@sendgrid/mail');
+mail.setApiKey(process.env.SENDGRID_SUBMISSIONS_KEY);
+
+const sendMail = (nameInput, emailInput, templateId, msgSubject) => {
+  const message = {
+    to: emailInput,
+    from: 'idasconf2018@protonmail.com',
+    templateId: templateId,
+    subject: msgSubject,
+    dynamic_template_data: {
+      fullName: nameInput,
+      email: emailInput,
+    },
+  };
+  mail
+    .send(message)
+    .then(() => {
+      console.log('Email sent');
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+export default sendMail;
