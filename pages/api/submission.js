683c23205de3e405817fc01aca10d9b0b539baea
Marge <marge.consunji@gmail.com>
2022-04-08 13:25:50 +0800

    Clean up code, import sendMail function

--- pages/api/submission.js
+++ pages/api/submission.js
@@ -1,25 +1,43 @@
-const mail = require('@sendgrid/mail');
-mail.setApiKey(process.env.SENDGRID_SUBMISSIONS_KEY);
+import middleware from '../../middleware/middleware';
+import nc from 'next-connect';
+import postAirtableData from '../../utils/postAirtableData';
+import sendMail from '../../utils/sendMail';
 
-export default function handler(req, res) {
-  const formData = JSON.parse(req.body);
-  const { fullName, email, org, select, file } = formData;
-  const message = `Hello ${fullName}!\r\n
-                   This is to confirm your registration for the 2018 IDAS International Conference happening on November 24-25 at National Chengchi University.
-                  `;
-  mail
-    .send({
-      to: email,
-      from: 'idasconf2018@protonmail.com',
-      subject: `Thank you for submitting your Paper!`,
-      text: message,
-    })
-    .then(() => {
-      console.log('Email sent');
-      res.status(200).json({ status: 'ok', body: req.body });
-    })
-    .catch((err) => {
-      res.json(err);
-      res.status(405).end();
-    });
+function onError(err, req, res, next) {
+  console.error(err);
+  res.status(500).end(err.toString());
+  next();
 }
+
+const handler = nc({ onError })
+  .use(middleware)
+  .post(async (req, res) => {
+    try {
+      const { fullName, email, org, select } = req.body;
+      let airtableData = {
+        'Full Name': fullName,
+        Email: email,
+        Organization: org,
+        Theme: select,
+      };
+
+      sendMail(
+        fullName,
+        email,
+        process.env.SENDGRID_SUBMISSIONS_TEMPLATE,
+        'IDAS Conference Abstract Submission Confirmation'
+      );
+      postAirtableData(process.env.SUBMISSIONS_TAB, airtableData);
+      res.status(200).json({ status: 'ok', body: req.body });
+    } catch (err) {
+      res.status(450).json({ status: 'error', error: err.message });
+    }
+  });
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default handler;
